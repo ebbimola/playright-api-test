74da12ff31526e5db0708d62d7da61aa626db103
Ademola Badmus <btinfo.demola@gmail.com>
2024-09-05 14:58:39 +0100

    github action and ui update

--- src/utils/env.js
+++ src/utils/env.js
@@ -2,14 +2,25 @@ const fs = require("fs");
 const path = require("path");
 const dotenv = require("dotenv");
 
+/**
+ * Loads environment variables from a specified .env file.
+ */
 function loadEnv() {
-  const envFilePath = path.resolve(__dirname, "../.env.local");
+  const envFilePath = path.resolve(__dirname, "../.env");
   if (fs.existsSync(envFilePath)) {
     dotenv.config({ path: envFilePath });
   }
 }
+
+/**
+ * Updates the environment variable with the specified key and value.
+ * 
+ * @param {string} key - The key of the environment variable to update.
+ * @param {string} value - The new value for the environment variable.
+ * @returns {void}
+ */
 function updateEnv(key, value) {
-  const envFilePath = path.resolve(__dirname, "../.env.local");
+  const envFilePath = path.resolve(__dirname, "../.env");
 
   const envVariables = fs.existsSync(envFilePath)
     ? dotenv.parse(fs.readFileSync(envFilePath))
